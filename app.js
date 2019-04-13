48780aefb41af3ddb76be22a16156376fe48966a
gui <gbrein@gmail.com>
2019-04-13 10:43:41 -0300

    criamos e salvamos um documento

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
-const mongoose = require('mongoose');
-const dbName = 'aula-mongoose';
-const Schema = mongoose.Scheema;
+const mongoose = require("mongoose");
+const dbName = "aula-mongoose";
+const Schema = mongoose.Schema;
 
 // Passo 1 - Conectamos ao banco de dados
-mongoose.connect(`mongodb://localhost/${dbName}`, (error) => {
+mongoose.connect(`mongodb://localhost/${dbName}`, error => {
   if (error) {
-    console.log('Não consegui conectar');
+    console.log("Não consegui conectar");
   } else {
     console.log(`CONECTAMOS EM ${dbName}`);
   }
 });
 
 //Passo 2 - Configurar o model
-const PersonModel = mongoose.model('Person', new Schema ({
-  name.String,
-  age.String,
-  gender.String
-}))
+const PersonModel = mongoose.model(
+  "Person",
+  new Schema({
+    name: String,
+    age: String,
+    gender: String
+  })
+);
+
+//Passo 3 - Configurando um documento
+const personDoc = {
+  name: "Pablo Escobar",
+  age: "40",
+  gender: "M"
+};
+
+//Passo 4 - Criando um Documento
+PersonModel.create(personDoc, error => {
+  if (error) {
+    console.log(`Erro ao criar documento ${error}`);
+  } else {
+    console.log(`Salvamos o documento: ${personDoc}`);
+  }
+});
