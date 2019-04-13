23e4b3aa3533f6c8078ba83134a556b5db12318e
Wilkor Almeida <wilkor.almeida@globalhitss.com.br>
2019-04-13 10:23:26 -0300

    conectando ao banco de dados

---
+++ app.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const dbName = 'aula-mongoose'
+
+// Passo 1 - Conectamos ao banco de dados
+mongoose.connect(`mongodb://localhost/${dbName}`, (error) => {
+
+if(error){
+   console.log('erro ao conectar');
+}else{
+   console.log(`conectado em ${dbName}`)
+}
+
+});
+
+
+
