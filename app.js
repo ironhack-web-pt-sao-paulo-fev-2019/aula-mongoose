cc797018e7ffa7b4ac77582f96f419d76a8815b5
gui <gbrein@gmail.com>
2019-04-13 10:31:44 -0300

    model criado

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const dbName = 'aula-mongoose';
+const Schema = mongoose.Scheema;
 
 // Passo 1 - Conectamos ao banco de dados
 mongoose.connect(`mongodb://localhost/${dbName}`, (error) => {
@@ -9,3 +10,10 @@ mongoose.connect(`mongodb://localhost/${dbName}`, (error) => {
     console.log(`CONECTAMOS EM ${dbName}`);
   }
 });
+
+//Passo 2 - Configurar o model
+const PersonModel = mongoose.model('Person', new Schema ({
+  name.String,
+  age.String,
+  gender.String
+}))
