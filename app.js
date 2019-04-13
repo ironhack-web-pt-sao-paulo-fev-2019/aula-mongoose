a76ea69c140d81aff23ae7cd7f16d41a7b67e6ed
José Luiz Coe <joseluizcoe@Joses-MacBook-Pro.local>
2019-04-13 10:31:06 -0300

    Criamos um model - referência para collection

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const dbName = 'aula-mongoose';
+const Schema = mongoose.Schema;
 
 // Passo 1 - Conectamos ao banco de dados
 mongoose.connect(`mongodb://localhost/${dbName}`, (error) => {
@@ -9,3 +10,12 @@ mongoose.connect(`mongodb://localhost/${dbName}`, (error) => {
     console.log(`CONECTAMOS EM ${dbName}`);
   }
 });
+
+// Passo 2 - Configurar Model
+const PersonModel = mongoose.model('Person', new Schema({
+  name: String,
+  age: Number,
+  gender: String
+}));
+
+
