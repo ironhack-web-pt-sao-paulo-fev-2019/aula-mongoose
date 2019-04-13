b804ff857968fefb2d9458b3a1532d25b17e040f
Wilkor Almeida <wilkor.almeida@globalhitss.com.br>
2019-04-13 10:43:28 -0300

    Salvando documento no banco de dados

--- app.js
+++ app.js
@@ -14,15 +14,29 @@ if(error){
 });
 
 // Passo 2 - configurar model
-
-
 const PersonModel = mongoose.model('Person', new Schema({
 
     name:String,
     age:String,
     gender:String
 
-}))
+}));
 
+// Pass 3 - criando um documento na collection
+const personDoc = {
 
+     name:'Wilkor',
+     age:'12',
+     genter:'M'
+}
+
+// Passo w - Criando um documento
+
+PersonModel.create(personDoc,(error) =>{
+    if(error){
+        console.log(`Erro ao criar documento ${error} `)
+    }else{
+        console.log(`Salvamos o documento: ${personDoc}`)
+    }
+})
 
