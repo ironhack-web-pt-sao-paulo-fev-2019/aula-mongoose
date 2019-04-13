63dd7505ae36eb2c19d2f748a4a5f08bc4b63927
fredconti <fred@conticonti.com>
2019-04-13 15:10:15 -0300

    Adicionei as rotas em arquivos separados

--- app.js
+++ app.js
@@ -1,36 +1,5 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const dbName = 'aula-mongoose';
-const PersonModel = require('./model/Person/PersonModel');
-const createPerson = require('./controller/Person/create');
-const readPerson = require('./controller/Person/read');
-const updatePerson = require('./controller/Person/update');
-const deletePerson = require('./controller/Person/delete');
+const app = require('./routes');
 
-mongoose.connect(`mongodb://192.168.0.26/${dbName}`, (error) => {
-  if (error) {
-    console.log('Não consegui conectar');
-  } else {
-    console.log(`CONECTAMOS EM ${dbName}`);
-  }
-});
-
-app.get('/', (request, response) => {
-  response.send('Minha primeira rota');
-});
-
-// C - Rota para criar
-app.get('/person/create', createPerson);
-
-// R - Rota para ler
-app.get('/person/read', readPerson);
-
-// U - Rota para atualizar
-app.get('/person/update/:personId', updatePerson );
-
-// D - Rota para apagar
-app.get('/person/delete/:personId', deletePerson );
 
 app.listen(3000, (error) => {
   if (error) {
@@ -38,6 +7,4 @@ app.listen(3000, (error) => {
   } else {
     console.log('App rodando na porta 3000');
   }
-  
 });
-
--- config.js
+++ config.js
@@ -1,25 +1,14 @@
 const express = require('express');
 const app = express();
-const hbs = require('hbs');
 const mongoose = require('mongoose');
 const dbName = 'fred-aula-mongoose';
 
-//Porta - Listen
-app.listen((3000), 'localhost', (error) => {
-    if(error) {
-        console.log("Deu ruim para conexão")
-    } else{
-        console.log("App rodando lisamente na porta 3000");
-    }
-});
-
-
 mongoose.connect(`mongodb://192.168.0.26/${dbName}`, (error) => {
-    if (error) {
-        console.log('Está dando pau!');
-    } else {
-        console.log(`Conectamos em ${dbName}!Talquei!`)
-    }
+  if (error) {
+    console.log('Não consegui conectar');
+  } else {
+    console.log(`CONECTAMOS EM ${dbName}`);
+  }
 });
 
 
