f6ea33a371c42880c6a5e054938f7670e40e08a5
Marcelo Oliveira <marbmo@gmail.com>
2019-04-13 10:22:00 -0300

    Conexão com banco de dados configurada

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+
+const dbName = 'marcelo-aula-mongoose';
+
+mongoose.connect(`mongodb://192.168.0.26/${dbName}`, (error) => {
+  if (error) {
+    console.log('Não consegui conectar');
+  } else {
+    console.log(`CONECTAMOS EM ${dbName}`);
+  }
+});
