58734b0c7aee8201b94435058a373534cc15e3c8
Marcelo Oliveira <marbmo@gmail.com>
2019-04-13 10:42:57 -0300

    Salvamos um documento no banco

--- app.js
+++ app.js
@@ -22,3 +22,19 @@ const PersonModel = mongoose.model('Person', new Schema({
   age: Number,
   gender: String,
 }));
+
+// passo 3 - Configurando um documento 
+const personDoc = {
+  name: 'Marcelo',
+  age: 32,
+  gender: 'Masculino',
+};
+
+// passo 4 - criando um documento
+PersonModel.create(personDoc, (error) => {
+  if (error) {
+    console.log(`Erro ao criar o documento: ${error}`);
+  } else {
+    console.log(`Salvamos o documento: ${personDoc}`);
+  }
+});
